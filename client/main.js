83e0aed808e6af2d11ad5b6e4aab9cdc604db6af
Brendan Bycroft <brendan.bycroft@gmail.com>
2015-06-17 23:26:21 +1200

    add lodash & verify that d3 works after dom load

--- client/main.js
+++ client/main.js
@@ -1,4 +1,11 @@
+var _ = require('lodash');
 var d3 = require('d3');
 
-console.log("Hello World");
+document.addEventListener('DOMContentLoaded', app);
 
+function app() {
+    _.each([1, 2, 54, 56, 3], function (x) {
+        console.log('value is ' + x);
+    });
+    d3.select('body').append('p').text('~ appended by d3 after DOM loaded ~');
+}
